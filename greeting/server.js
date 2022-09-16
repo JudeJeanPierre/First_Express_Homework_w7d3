6c1b565f7ed17375f8b5807ed180bfeb05d60ac9
JudeJeanPierre <110211488+JudeJeanPierre@users.noreply.github.com>
2022-09-16 00:06:33 -0500

    3rd commit

--- greeting/server.js
+++ greeting/server.js
@@ -2,6 +2,30 @@ const express = require ('express')
 const app = express()
 const ejs = require ('ejs')
 
+// Magic 8 ball
+const answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+    ];
+    
 app.get('/greeting', (req, res) =>{
     res.send('Hello, Stranger')
 })
@@ -17,6 +41,14 @@ app.get('/tip/:total/:tipPercentage', (req, res) =>{
         res.send(tipTotal)
     })
 
+// Magic ball route
+app.get('/magic', (req, res) =>{
+    res.send('Ask a question in the url')
+})
+app.get('/magic/:question', (req, res) =>{
+    let randomAnswer = answers[Math.floor(Math.random()*answers.length)]
+    res.send(req.params.question + '? ' + ' Answer: ' + randomAnswer)
+})
 
 
 
