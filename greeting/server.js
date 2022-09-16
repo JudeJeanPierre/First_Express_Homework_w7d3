3215e2f4b00be19bda02e4676a0e594fa4a8a7c1
JudeJeanPierre <110211488+JudeJeanPierre@users.noreply.github.com>
2022-09-16 07:37:54 -0500

    5th commit

--- greeting/server.js
+++ greeting/server.js
@@ -50,6 +50,25 @@ app.get('/magic/:question', (req, res) =>{
     res.send(req.params.question + '? ' + ' Answer: ' + randomAnswer)
 })
 
+// Fibonacci
+app.get('/fibonacci/:num/', (req, res) =>{
+    let fibo = [0, 1]
+    for(i = 2; i<100; i++){
+        newFibo = fibo[i-1] + fibo[i-2]
+        fibo.push(newFibo)
+    }
+    let message = 0
+    fibo.forEach(el =>{
+        if(parseInt(req.params.num) === el){
+            message +=1
+        }
+    })
+    if(message > 0){
+        res.send("Very good. It is Fibonacci.")
+    }else{
+        res.send("I can tell this is not a fibonacci number")
+    }
+})
 
 
 
