66cbee905baeb0cb1d5273b41b0883464a6aa3fc
JudeJeanPierre <110211488+JudeJeanPierre@users.noreply.github.com>
2022-09-15 23:43:41 -0500

    2nd commit

--- greeting/server.js
+++ greeting/server.js
@@ -9,6 +9,13 @@ app.get('/greeting', (req, res) =>{
 app.get('/greeting/:name', (req, res) =>{
     res.send('Hello ' + req.params.name + '!' +' It is so great to see you.' )
 })
+// Tip
+app.get('/tip/:total/:tipPercentage', (req, res) =>{
+        let total = parseInt(req.params.total)
+        let tipPercentage = parseInt(req.params.tipPercentage)
+        let tipTotal = (total*(tipPercentage/100)).toString()
+        res.send(tipTotal)
+    })
 
 
 
